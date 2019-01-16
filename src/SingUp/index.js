f67a4a4cefb59ae4b18a17af3e0d141fc761dfad
YarynkaMazuryk <yarynkafox@gmail.com>
2019-01-16 12:53:51 +0200

    add basic form

--- src/SingUp/index.js
+++ src/SingUp/index.js
@@ -6,26 +6,38 @@ class SingUp extends PureComponent {
   constructor(props){
     super(props);
     this.state = {
-      name: ''
+      name: '',
+      email: ''
     }
   }
 
   checkName(e) {
-    console.log(e.target.value);
     e.preventDefault();
     const name = e.target.value;
-    if (name) {
-
+    if (name.length === 4) {
+      this.setState({name: name});
+      e.target.classList.remove('error')
+    } else  {
+      e.target.classList.add('error')
     }
-    this.setState({name: name});
+  }
+  checkEmail(e) {
+    e.preventDefault();
+    const email = e.target.value;
+    if (email.indexOf('@') !== -1) {
+      this.setState({email: email});
+      e.target.classList.remove('error');
+    } else  {
+      e.target.classList.add('error');
+    }
   }
   render () {
     return ( <form className='singUp'>
         <input type="text" placeholder='Enter the name' onChange = {(e)=> this.checkName(e)}/>
-        {/*<input type="email" placeholder='Enter the email'/>*/}
-        {/*<input type="text" placeholder='Enter the description'/>*/}
-        {/*<input type="file"/>*/}
-        {/*<input type="text" placeholder = 'Enter your status'/>*/}
+        <input type="email" placeholder='Enter the email' onChange = {(e)=> this.checkEmail(e)}/>
+        <input type="text" placeholder='Enter the description'/>
+        <input type="file"/>
+        <input type="text" placeholder = 'Enter your status'/>
         <input type="submit" id='submit'/>
       </form>
     )
